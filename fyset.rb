575880f666c0448fae84337363db1f511aa71d50
Сергей Гомон <sggomon@gmail.com>
2018-12-21 15:00:13 +0100

    add formula

---
+++ fyset.rb
@@ -0,0 +1,15 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Installer < Formula
+  desc "The installer for fyset setup"
+  homepage ""
+  url "https://github.com/fyset/installer/archive/v1.0.1.tar.gz"
+  sha256 "957fe96f209237741734e32989233aeb12dcf38d5145fe5613347b41eba55990"
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "fyset"
+  end
+
+end
